ceacd407bfd508f15128b04834d70e2a59e45b4a
CristinaDAmico <crydamico94@gmail.com>
2021-06-15 14:40:32 +0200

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1>Welcome to our Home Page!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod delectus placeat sit cum beatae iste, sed numquam cumque, amet quo non minima laborum, enim accusantium dolores rerum. Voluptatum, rem iusto!</p>
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="mt-5 text-center">
+    Comics &hearts; for comics lovers!
+</footer>
